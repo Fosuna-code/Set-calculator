1ece4febdb857a998f4d289543988728b4abd03e
PetuchoDev <kevin.varick@gmail.com>
2024-04-24 23:20:09 -0700

    Made the diagrams colorful

--- src/components/Venn.js
+++ src/components/Venn.js
@@ -5,18 +5,37 @@ import { SetContext } from "../Context";
 import { VennDiagram } from "venn.js";
 
 import * as d3 from "d3";
+const colors = []
+
 function Venn(){
     const context = useContext(SetContext);
-
+    const getRandomHex = ()=>{
+        let r = Math.round(Math.random()*255).toString(16)
+        let g = Math.round(Math.random()*255).toString(16)
+        let b = Math.round(Math.random()*255).toString(16)
+        let hex = r+g+b
+        return `#${hex}`
+    }
+    
     useEffect(() => {
         var sets = context.drawInstructions();
         
         var chart = VennDiagram()
         d3.select("#venn").datum(sets).call(chart);
-
+        
+        console.log(d3.selectAll('#venn path')._groups[0][0]?.attributes.style.value)
+        
+        for(let i=0; i<context.setelements.length; i++){
+            if(colors.length <= i){
+                colors.push(getRandomHex())
+            }
+            console.log(colors)
+            d3.selectAll('#venn path')._groups[0][i].attributes.style.value = `fill-opacity: 0.3; fill: ${colors[i]} `
+        }
       //Every time the context changes, the diagram does aswell
       //or every time the set sizes are altered, a render is forced
       }, [context.setelements]); 
+
     return(
         <div id="venn"> 
         </div>
