70983afe3f86ca5ebb8c165c03a3e8995fe9e43e
PetuchoDev <kevin.varick@gmail.com>
2024-05-12 20:23:59 -0700

    Added parenthesis functionality, fixed input cursor bug

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,9 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useRef, useEffect } from 'react'
 import { SetContext } from '../Context'
 
 export default function Keyboard() {
-  //used to display the input value when an input is added from a button
-  const [inputVal, setInputVal] = useState('')
+
+  const context = useContext(SetContext)
+  //used to display the input value when an input is added from a button and move the cursor to a correct position
+  const [inputVal, setInputVal] = useState({
+    val: '',
+    pos: 0
+  })
   //used to see what to display or not in order to diminish the amount of invalid inputs possible
   const [keyboardState, setKeybState] = useState({
     setpad: 'show',
@@ -11,27 +16,67 @@ export default function Keyboard() {
     complement: 'hide',
     ispartOf: 'hide'
   })
-  //stores what operations are special in terms that they cannot be combined with other sets or themselves
-  const specialOperations = ["'", '∈']
+  const inputref = useRef(null)
+  //used to move the cursor around the input:
+  const moveCursor = (position)=>{
+    if(inputref.current){
+      inputref.current.setSelectionRange(position, position)
+    }
+  }
   //this functions validates on-time the inputs, right now it just tests what the input contains
   const handleChange = (e)=>{
     let inputValue = e.target.value
-    //updates the state
-    setInputVal(()=>inputValue)
-    //these are regular expressions used to validate many forms of input
-    const setRegexp = /\D+/g
-    const intersectionRegexp = /\D*(?:\D'?∩\D'?[^∪-△]?)+/g
-    const unionRegexp = /\D*(?:\D'?∪\D'?[^∩-△]?)+/g
-    const complementRegexp = /\D'/g
-    const differenceRegexp = /\D*(?:\D'?-\D'?[^∪∩△]?)+/g
-    const simmetricDifferenceRegexp = /\D*(?:\D'?△\D'?[^∪∩-]?)+/g
+    let updatedCursorPosition = e.target.selectionStart
+    //these are regular expressions used to validate some forms of input
+    const setRegexp = /[^\(\)"]+/g
     const elementRegexp = /"\D+"/g
+    //sees if a character has been deleted to update the keyboard and show the inputs that are supposed to be shown before that character was deleted
+    if(inputValue.length < inputVal?.val.length){
+      let extraValue = inputValue.split('').pop()
+      if(extraValue === "'"){
+        setKeybState({
+          setpad: 'hide',
+          oppad:'show',
+          complement: 'show',
+          ispartOf: 'hide'
+        })
+      } else if(keyboardState.setpad === 'show'){
+        setKeybState({
+          setpad: 'hide',
+          oppad:'show',
+          complement: 'show',
+          ispartOf: 'hide'
+        })
+      } else{
+        setKeybState({
+          setpad: 'show',
+          oppad:'hide',
+          complement: 'hide',
+          ispartOf: 'hide'
+        })
+      }
+    }
+    //if the most recent input was a parenthesis and nothing has been deleted close it automatically
+    if((/[\(]?/g).test(inputValue) && inputValue.length >= inputVal?.val.length){
+      let mostRecentValue = inputValue.split('')[updatedCursorPosition - 1]
+      if(mostRecentValue === "(") {
+        inputValue += ')'
+      }
+    }
     //if the first input is a letter, it blocks the setpad because it is a '"' or it is a setName, either way there shouldn't be a set as an input afther both ocurrences
     if(setRegexp.test(inputValue)){
       setKeybState({
         setpad: 'hide',
         oppad:'show',
         complement: 'show',
+        ispartOf: 'hide'
+      })
+      //if the input is a '"' it should block every input until it's closed
+    }else if((/"/g).test(inputValue)){
+      setKeybState({
+        setpad: 'hide',
+        oppad:'hide',
+        complement: 'hide',
         ispartOf: 'hide'
       })
     }
@@ -44,60 +89,90 @@ export default function Keyboard() {
         ispartOf: 'show'
       })
     }
-    
-    // console.log('im doing things xd, btw this is your input: ', inputValue)
-    // console.log('aand this is the intersection Regexp test:', inputValue.match(intersectionRegexp))
-    // console.log('aand this is the union Regexp test:', inputValue.match(unionRegexp))
-    // console.log('aand this is the complement Regexp test:', inputValue.match(complementRegexp))
-    // console.log('aand this is the difference Regexp test:', inputValue.match(differenceRegexp))
-    // console.log('aand this is the simmetric difference Regexp test:', inputValue.match(simmetricDifferenceRegexp))
-    // console.log('aand this is the element Regexp test:', inputValue.match(elementRegexp))
+    //updates the state
+    setInputVal(()=>{
+      return {
+        val: inputValue,
+        pos: updatedCursorPosition
+      }
+    })
   }
   //this adds a value to the state depending on the button of the calculator for it to be displayed
-  const addInputVal = (val, shown)=>{
+  const addInputVal = async (val, shown)=>{
     //this evaluates if the button is not hidden at the moment
     if(shown == 'show'){
-      setInputVal(inputVal + val)
-      //if its a special operation there has to be a normal operation next but not another special one
-      if(specialOperations.indexOf(val) !== -1){
-        setKeybState({
-          setpad: 'show',
-          oppad:'hide',
-          complement: 'hide',
-          ispartOf: 'hide'
+      let inputValues = inputVal.val.split('')
+      //updates the input value and the cursor position
+      if(inputref.current) {
+        inputref.current.focus()
+        let updatedCursorPosition = inputref.current.selectionStart
+        inputValues.length > 0 ? inputValues[updatedCursorPosition -1] += val : inputValues[0] = val
+        console.log(updatedCursorPosition)
+        await setInputVal(()=>{
+          return {val: inputValues.join(''),
+          pos: updatedCursorPosition+= 1
+          }
         })
-        //if the setpad is shown, hide it (it only happens when a setpad button is pressed)
-      }else if(keyboardState.setpad=='show'){
-        setKeybState({
-          setpad: 'hide',
-          oppad:'show',
-          complement: 'show',
-          ispartOf: 'hide'
-        })
+        console.log(inputVal.pos)
       }
-      //if there is no ocurrence, revert to original state
-      else{
-        setKeybState({
-          setpad: 'show',
-          oppad:'hide',
-          complement: 'hide',
-          ispartOf: 'hide'
-        })
+      //if the input is a parenthesis, preserve the previous state
+      if(val === "()"){
+        setKeybState(()=>keyboardState)
+      }
+      else {  
+        //if its a complement operation there has to be a normal operation next but not another complement one
+        if(val === "'"){
+          setKeybState({
+            setpad: 'hide',
+            oppad:'show',
+            complement: 'hide',
+            ispartOf: 'hide'
+          })
+          //if the setpad is shown, hide it (it only happens when a setpad button is pressed)
+        }else if(keyboardState.setpad=='show'){
+          setKeybState({
+            setpad: 'hide',
+            oppad:'show',
+            complement: 'show',
+            ispartOf: 'hide'
+          })
+        }
+        //if there is no ocurrence, revert to original state
+        else{
+          setKeybState({
+            setpad: 'show',
+            oppad:'hide',
+            complement: 'hide',
+            ispartOf: 'hide'
+          })
+        }
       }
     }
     
   }
-  const context = useContext(SetContext)
 
   const makeOperation = (e)=>{
     e.preventDefault()
     const inputValue = e.target.elements['keyboard-input'].value
+    const intersectionRegexp = /\D*(?:\D'?∩\D'?[^∪-△]?)+/g
+    const unionRegexp = /\D*(?:\D'?∪\D'?[^∩-△]?)+/g
+    const complementRegexp = /\D'/g
+    const differenceRegexp = /\D*(?:\D'?-\D'?[^∪∩△]?)+/g
+    const simmetricDifferenceRegexp = /\D*(?:\D'?△\D'?[^∪∩-]?)+/g
     //it currently does nothing, is only used to prevent a form submission
+    //and it also has regular expressions that will be used in the future to validate the input and do de algebra
   }
+  //changes the cursor position to where it should be after an input is added
+  useEffect(()=>{
+    setTimeout(()=>{
+      moveCursor(inputVal.pos)
+    })
+  },[inputVal.val])
+
   return (
     <div id="setkeyboard">
       <form id='keyboard-form' onSubmit={makeOperation}>
-        <input type="text" id='keyboard-input' value={inputVal} onChange={handleChange}></input>
+        <input ref={inputref} type="text" id='keyboard-input' value={inputVal.val} onChange={handleChange}></input>
         <button id='send-keyboard' type='submit'>Make Operation</button>
       </form>
       <div id='keyboard-btns'>
@@ -115,9 +190,10 @@ export default function Keyboard() {
           <button className={`keyboardbtn ${keyboardState.complement}`} onClick={()=>{addInputVal("'", keyboardState.complement)}}>'</button>
           <button className={`keyboardbtn ${keyboardState.oppad}`} onClick={()=>{addInputVal('-', keyboardState.complement)}}>-</button>
           <button className={`keyboardbtn ${keyboardState.oppad}`} onClick={()=>{addInputVal('△', keyboardState.oppad)}}>△</button>
-          <button  className={`keyboardbtn ${keyboardState.oppad}`} onClick={()=>{addInputVal('N()', keyboardState.oppad)}}>{`N()`}</button>
+          <button className={`keyboardbtn ${keyboardState.oppad}`} onClick={()=>{addInputVal('N()', keyboardState.oppad)}}>{`N()`}</button>
           <button className={`keyboardbtn ${keyboardState.oppad}`} onClick={()=>{addInputVal("⊂", keyboardState.oppad)}}>⊂</button>
           <button className={`keyboardbtn ${keyboardState.ispartOf}`} onClick={()=>{addInputVal("∈", keyboardState.ispartOf)}}>∈</button>
+          <button className={`keyboardbtn`} onClick={()=>{addInputVal("()", 'show')}}>()</button>
         </div>
         
         
